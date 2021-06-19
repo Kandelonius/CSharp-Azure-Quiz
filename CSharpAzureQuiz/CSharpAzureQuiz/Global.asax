689aeb97b6e79624442cb7bb5f5bf8664fcf6f62
Shane Kaestner <shane.kaestner@yahoo.com>
2021-06-19 15:32:06 -0700

    initial commit

---
+++ CSharpAzureQuiz/CSharpAzureQuiz/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CSharpAzureQuiz.MvcApplication" Language="C#" %>
---
+++ CSharpAzureQuiz/CSharpAzureQuiz/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CSharpAzureQuiz.Startup))]
+namespace CSharpAzureQuiz
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
